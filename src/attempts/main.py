fb1f24374a2c6ddf5050b302697ee05d2b779e4c
Pedro Henrique Araujo <pedro.tcp@gmail.com>
2024-09-07 22:30:01 -0300

    fix default color

--- src/attempts/main.py
+++ src/attempts/main.py
@@ -6,7 +6,7 @@ attempts_instance = None
 default_timestamp_start = "["
 default_timestamp_end = "]"
 default_timestamp_format = "%H:%M:%S.%f"
-default_timestamp_color = Fore.GREEN
+default_timestamp_color = "WHITE"
 
 
 class Attempts:
